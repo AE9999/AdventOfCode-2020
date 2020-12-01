79cdc45f2b0206aeff2a18430d73336db6629c36
AE <ae@ae.nl>
2020-12-01 06:33:32 +0100

    Sollution for day 1

--- day-01/src/main/kotlin/com/ae/aoc2020/day01/Aoc2020ApplicationDay01.kt
+++ day-01/src/main/kotlin/com/ae/aoc2020/day01/Aoc2020ApplicationDay01.kt
@@ -13,8 +13,51 @@ class Aoc2020ApplicationDay01 : CommandLineRunner {
 
 	private val logger = LoggerFactory.getLogger(javaClass)
 
+	private fun solve1(totalFile: String) {
+		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay01::class.java.getResourceAsStream("/$totalFile")))
+
+		bufferedReader.useLines {
+			val numbers = it.map { it.toInt() }.toList()
+			val pairs = sequence {
+				for (i in 0 until numbers.count()) {
+					for (j in (i + 1) until numbers.count()) {
+						yield(Pair(numbers[i], numbers[j]))
+					}
+				}
+			}.toList()
+			pairs.filter { pair -> pair.first +  pair.second ==  2020}
+					 .forEach {pair ->
+				logger.info("$pair sums to 2020 and multiply to ${pair.first * pair.second} ...")
+			}
+		}
+	}
+
+	private fun solve2(totalFile: String) {
+		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay01::class.java.getResourceAsStream("/$totalFile")))
+
+		bufferedReader.useLines {
+			val numbers = it.map { it.toInt() }.toList()
+			val pairs = sequence {
+				for (i in 0 until numbers.count()) {
+					for (j in (i + 1) until numbers.count()) {
+						for (k in (j + 1) until numbers.count() ) {
+							yield(Triple(numbers[i], numbers[j], numbers[k]))
+						}
+					}
+				}
+			}.toList()
+			pairs.filter { triple -> triple.first +  triple.second +  triple.third ==  2020}
+							.forEach {triple ->
+								val multiple = triple.first * triple.second * triple.third
+								logger.info("$triple sums to 2020 and multiply to $multiple ...")
+							}
+		}
+	}
+
+
 	override fun run(vararg args: String?) {
-		logger.info("Hello World")
+		solve1(args[0]!!)
+		solve2(args[0]!!)
 	}
 }
 
