18cfbaee4a0aec291306f83db09c34ace27c99cf
AE <ae@ae.nl>
2020-12-11 10:07:35 +0100

    Solves day 11, part 2

--- day-11/src/main/kotlin/com/ae/aoc2020/day11/Aoc2020ApplicationDay11.kt
+++ day-11/src/main/kotlin/com/ae/aoc2020/day11/Aoc2020ApplicationDay11.kt
@@ -33,8 +33,35 @@ class Aoc2020ApplicationDay11 : CommandLineRunner {
 					.filter { y + it.second >= 0 && y + it.second < height()  }
 					.map {
 						rows[y + it.second][x + it.first]
-					}.toList()
-					.toList()
+					}
+			 .toList()
+		}
+
+		private fun calculateStuff(x: Int, y: Int, d: Pair<Int, Int>) : Char {
+			var x_ = x
+			var y_ = y
+			while (true) {
+				x_ += d.first
+				y_ += d.second
+				if (x_ < 0 || x_ >= width() || y_ < 0 || y_ >= height()) {
+					return '.'
+				}
+				if (rows[y_][x_] != '.') return rows[y_][x_]
+			}
+		}
+
+		private fun seen(x: Int, y: Int) : List<Char> {
+			val trajectories = sequence {
+				listOf(-1, 1, 0).forEach { dx ->
+					listOf(-1, 1, 0).forEach { dy ->
+						yield(Pair(dx,dy))
+					}
+				}
+			}.filter { it.first != 0 || it.second != 0 }.toList()
+
+			return trajectories.map { d ->
+				calculateStuff(x, y, d)
+			}
 		}
 
 		fun occupiedSeats() =
@@ -54,6 +81,22 @@ class Aoc2020ApplicationDay11 : CommandLineRunner {
 			}.toList()
 			rows = nextState
 		}
+
+		fun step2() {
+			val nextState = rows.indices.map { y ->
+				val row = rows[y]
+				row.indices.map { x ->
+					when(row[x]) {
+						'.' -> '.'
+						'L' -> if (seen(x, y).none { it == '#' }) '#' else 'L'
+						'#' -> if (seen(x, y).filter { it == '#' }.size >= 5) 'L' else '#'
+						else -> RuntimeException("Bullshit ${row[x]}")
+					}
+				}.joinToString("")
+			}.toList()
+			rows = nextState
+		}
+
 
 		fun dump() {
 			logger.info("STARTING DUMP..")
@@ -82,6 +125,14 @@ class Aoc2020ApplicationDay11 : CommandLineRunner {
 	private fun solve2(totalFile: String) {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay11::class.java.getResourceAsStream("/$totalFile")))
 		bufferedReader.useLines {
+			val seatingSystem = SeatingSystem(it.toList())
+			var occupiedSeats = seatingSystem.occupiedSeats()
+			seatingSystem.step2()
+			while (seatingSystem.occupiedSeats() != occupiedSeats) {
+				occupiedSeats = seatingSystem.occupiedSeats()
+				seatingSystem.step2()
+			}
+			logger.info("${occupiedSeats} seats end up occupied")
 		}
 	}
 
