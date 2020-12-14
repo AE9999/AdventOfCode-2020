7c4ea7368eb6fd4711e538d5bd86ca348cafe7f5
AE <ae@ae.nl>
2020-12-14 09:39:05 +0100

    Solves part 2 of day 14

--- day-14/src/main/kotlin/com/ae/aoc2020/day14/Aoc2020ApplicationDay14.kt
+++ day-14/src/main/kotlin/com/ae/aoc2020/day14/Aoc2020ApplicationDay14.kt
@@ -7,24 +7,20 @@ import org.springframework.boot.runApplication
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.RuntimeException
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.sin
 
 @SpringBootApplication
 class Aoc2020ApplicationDay14 : CommandLineRunner {
 
 	private val logger = LoggerFactory.getLogger(javaClass)
 
+	fun long2string(value: Long) : String = value.toString(2).padStart(36, '0')
+
+	fun string2long(value: String) : Long = value.toLong(2)
+
 	inner class Program() {
 		val program = HashMap<Long, String>()
 
-		var currentMask = "101XX10X1X00001010011011X1XXX1001011"
-
-		fun long2string(value: Long) : String = value.toString(2).padStart(36, '0')
-
-		fun string2long(value: String) : Long = value.toLong(2)
-
+		var currentMask = ""
 
 		fun applyMask(value: String) : String {
 			return value.toCharArray().withIndex().map {
@@ -35,12 +31,60 @@ class Aoc2020ApplicationDay14 : CommandLineRunner {
 			}.joinToString("")
 		}
 
-
 		fun applyInstruction(line: String) {
 			if(line.startsWith("mem")) {
 				val (destination, value) = Regex("mem\\[(\\d+)\\] = (\\d+)").find(line)!!.destructured
 				program[destination.toLong()] = applyMask(long2string(value.toLong()))
 
+			} else if (line.startsWith("mask")) {
+				currentMask = line.split(" ")[2]
+			}
+		}
+
+		fun memValue() : Long = program.map { string2long(it.value) }.sum()
+	}
+
+	inner class ProgramV2() {
+		val program = HashMap<Long, String>()
+
+		var currentMask = ""
+
+		fun applyMask(address: String) : List<Long> {
+			val size = currentMask.filter { it == 'X' }.length.toDouble()
+			val index2position = currentMask.withIndex()
+					                        .filter { it.value == 'X' }
+											.withIndex()
+											.map { Pair(it.value.index, it.index) }
+											.toMap()
+			val maskedAddress = address.toCharArray().withIndex().map {
+				when(currentMask[it.index]) {
+					'1', 'X' -> currentMask[it.index]
+					'0' -> it.value
+					else -> throw RuntimeException("Illegal Input")
+				}
+			}
+			
+			return (0..(Math.pow(2.0, size).toInt())).map {
+				val repValues = it.toLong().toString(2).padStart(size.toInt(), '0').toCharArray()
+				val newValue = maskedAddress.toCharArray().withIndex().map {
+					when (it.value) {
+						'1', '0' -> it.value
+						'X' -> {
+							repValues[index2position[it.index]!!]
+						}
+						else -> throw RuntimeException("Illegal Input")
+					}
+				}.joinToString("")
+				newValue.toLong(2)
+			}
+		}
+
+		fun applyInstruction(line: String) {
+			if(line.startsWith("mem")) {
+				val (destination, value) = Regex("mem\\[(\\d+)\\] = (\\d+)").find(line)!!.destructured
+				applyMask(long2string(destination.toLong())).forEach { d ->
+					program[d] = long2string(value.toLong())
+				}
 			} else if (line.startsWith("mask")) {
 				currentMask = line.split(" ")[2]
 			}
@@ -64,6 +108,11 @@ class Aoc2020ApplicationDay14 : CommandLineRunner {
 	private fun solve2(totalFile: String) {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay14::class.java.getResourceAsStream("/$totalFile")))
 		bufferedReader.useLines {
+			val program = ProgramV2()
+			it.forEach { line ->
+				program.applyInstruction(line)
+			}
+			logger.info("${program.memValue()} is the sum of all values left in memory after it completes ..")
 		}
 	}
 
