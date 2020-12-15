2288b54eec93af8d3611cb00f787eb0443428c0e
AE <ae@ae.nl>
2020-12-15 07:39:20 +0100

    Solves part 1 of day 15

---
+++ day-15/src/main/kotlin/com/ae/aoc2020/day15/Aoc2020ApplicationDay15.kt
@@ -0,0 +1,59 @@
+package com.ae.aoc2020.day15
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+@SpringBootApplication
+class Aoc2020ApplicationDay15 : CommandLineRunner {
+
+	private val logger = LoggerFactory.getLogger(javaClass)
+
+	private fun play(numbers: List<Int>) : Int {
+		val spokenNumbers = HashMap<Int, Set<Int>>()
+		var turn  = 1
+		var spoken = -1
+		numbers.forEach {
+			spoken = it
+			spokenNumbers[spoken] = spokenNumbers.getOrDefault(spoken, emptySet()).plus(turn)
+			turn++
+		}
+		while (turn <= 2020) {
+			val timesSpokenBefore = spokenNumbers[spoken]!!.filter { it != (turn - 1) }
+			spoken = if (timesSpokenBefore.isEmpty()) 0
+			         else (turn - 1) - timesSpokenBefore.max()!!
+			spokenNumbers[spoken] = spokenNumbers.getOrDefault(spoken, emptySet()).plus(turn)
+			turn++
+		}
+		return spoken
+	}
+
+	private fun solve1(totalFile: String) {
+		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay15::class.java.getResourceAsStream("/$totalFile")))
+		bufferedReader.useLines { input ->
+			input.forEach { line ->
+				logger.info("${play(line.split(",").map { it.toInt() })} will be the 2020th number spoken")
+			}
+		}
+	}
+
+	
+	private fun solve2(totalFile: String) {
+		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay15::class.java.getResourceAsStream("/$totalFile")))
+		bufferedReader.useLines {
+		}
+	}
+
+	override fun run(vararg args: String?) {
+		solve1(args[0]!!)
+		solve2(args[0]!!)
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<Aoc2020ApplicationDay15>(*args)
+}
