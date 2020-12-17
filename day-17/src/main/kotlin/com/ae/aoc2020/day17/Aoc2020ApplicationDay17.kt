029b2dfb3d38b5d549b9d1102930c6903bd3649f
AE <ae@ae.nl>
2020-12-17 09:31:36 +0100

    That I had to refactor

--- day-17/src/main/kotlin/com/ae/aoc2020/day17/Aoc2020ApplicationDay17.kt
+++ day-17/src/main/kotlin/com/ae/aoc2020/day17/Aoc2020ApplicationDay17.kt
@@ -13,8 +13,12 @@ class Aoc2020ApplicationDay17 : CommandLineRunner {
 
 	private val logger = LoggerFactory.getLogger(javaClass)
 
-	data class FourDCoordinate(val x: Int, val y: Int, val z: Int, val w: Int) {
-		fun neighbours(): Set<FourDCoordinate> {
+	interface Manhattan<T> {
+		fun neighbours(): Set<Manhattan<T>>
+	}
+
+	data class FourDCoordinate(val x: Int, val y: Int, val z: Int, val w: Int) : Manhattan<FourDCoordinate> {
+		override fun neighbours(): Set<FourDCoordinate> {
 			val ofsets = listOf(-1, 0, 1)
 			val seq = sequence {
 				ofsets.forEach { dx->
@@ -31,11 +35,29 @@ class Aoc2020ApplicationDay17 : CommandLineRunner {
 			}
 			return seq.toSet()
 		}
+	}
+
+	data class MyTriplet(val first: Int, val second: Int, val third: Int) : Manhattan<MyTriplet> {
+		override fun neighbours(): Set<MyTriplet> {
+			val ofsets = listOf(-1, 0, 1)
+			val seq = sequence {
+				ofsets.forEach { dx->
+					ofsets.forEach { dy->
+						ofsets.forEach { dz->
+							if (dx != 0 || dy != 0 || dz != 0) {
+								yield(MyTriplet(first + dx, second + dy, third + dz ))
+							}
+						}
+					}
+				}
+			}
+			return seq.toSet()
+		}
 
 	}
 
-	inner class HyperCubeConwayCubeSystem(var activeCubes: Set<FourDCoordinate>) {
-		private fun calculateInactiveCubes() : Set<FourDCoordinate> {
+	inner class HyperCubeConwayCubeSystem<T>(var activeCubes: Set<Manhattan<T>>) {
+		private fun calculateInactiveCubes() : Set<Manhattan<T>> {
 			return activeCubes.map { it.neighbours() }
 					.flatten()
 					.filter { candidate -> !activeCubes.contains(candidate) }.toSet()
@@ -59,41 +81,14 @@ class Aoc2020ApplicationDay17 : CommandLineRunner {
 		fun nactive() = activeCubes.size
 	}
 
-	inner class ConwayCubeSystem(var activeCubes: Set<Triple<Int, Int, Int>>) {
-
-		private fun calculateInactiveCubes() : Set<Triple<Int, Int, Int>> {
-			return activeCubes.map { it.neighbours() }
-					          .flatten()
-					          .filter { candidate -> !activeCubes.contains(candidate) }.toSet()
-		}
-
-		fun step() {
-			val cubesToDeactivate = activeCubes.filter { cube ->
-				cube.neighbours().filter { neighbour ->
-					activeCubes.contains(neighbour)
-				}.size !in 2..3
-			}
-			val cubesToActivate = calculateInactiveCubes().filter {
-				it.neighbours().filter { neighbour ->
-					 activeCubes.contains(neighbour)
-				}.size == 3
-			}
-			if (cubesToDeactivate.intersect(cubesToActivate).isNotEmpty()) throw RuntimeException("Should not intersect ..")
-			activeCubes = activeCubes.minus(cubesToDeactivate).plus(cubesToActivate)
-		}
-
-		fun nactive() = activeCubes.size
-	}
-
-
 	private fun solve1(file: String) {
 		val bufferedReader = BufferedReader(InputStreamReader(Aoc2020ApplicationDay17::class.java.getResourceAsStream("/$file")))
 		bufferedReader.useLines { input ->
-			val conwayCubeSystem = ConwayCubeSystem(input.withIndex()
+			val conwayCubeSystem = HyperCubeConwayCubeSystem(input.withIndex()
 					                                     .map { yLine ->
 				                                                 yLine.value.withIndex()
 																		    .filter { it.value == '#' }
-																		     .map { xLine ->  Triple(xLine.index, yLine.index, 0) }
+																		     .map { xLine ->  MyTriplet(xLine.index, yLine.index, 0) }
 			                                                  }
 					                                     .flatten()
 					                                     .toSet())
@@ -127,22 +122,6 @@ class Aoc2020ApplicationDay17 : CommandLineRunner {
 		solve1(args[0]!!)
 		solve2(args[0]!!)
 	}
-}
-
-private fun Triple<Int, Int, Int>.neighbours(): Set<Triple<Int, Int, Int>> {
-	val ofsets = listOf(-1, 0, 1)
-	val seq = sequence {
-		ofsets.forEach { dx->
-			ofsets.forEach { dy->
-				ofsets.forEach { dz->
-					if (dx != 0 || dy != 0 || dz != 0) {
-						yield(Triple(first + dx, second + dy, third + dz ))
-					}
-				}
-			}
-		}
-	}
-	return seq.toSet()
 }
 
 fun main(args: Array<String>) {
